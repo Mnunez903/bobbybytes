a8ca03aab7a29843b14b7b2eb113ad4b290c641a
Michael Nunez <mnunez903@gmail.com>
2024-03-15 13:22:24 -0400

    Add some detail to bobbybytes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import underConstruction from './assets/images/under-construction.png';
 
 function App() {
 
   return (
-    <div className='text-3xl font-bold underline'>
-      hello world
+    <div className='h-screen bg-orange-300 flex items-center justify-center flex-col'>
+      <h1 className='text-5xl'>Bobby Bytes</h1>
+      <img className='h-64' src={underConstruction}/>
+      <h1 className='text-5xl'>Under Construction</h1>
     </div>
   )
 }
